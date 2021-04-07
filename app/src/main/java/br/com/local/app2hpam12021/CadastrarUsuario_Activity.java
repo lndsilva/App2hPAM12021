72abb186705917c6605b6751262e8efb0e4743cb
Laercio-Note <lndsilva@hotmail.com>
2021-04-07 20:24:36 -0300

    autenticação e janela de cadastro

---
+++ app/src/main/java/br/com/local/app2hpam12021/CadastrarUsuario_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.app2hpam12021;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastrarUsuario_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastrar_usuario_layout);
+    }
+}
--- app/src/main/java/br/com/local/app2hpam12021/Login_Activity.java
+++ app/src/main/java/br/com/local/app2hpam12021/Login_Activity.java
@@ -1,33 +1,69 @@
 //Pacotes
 package br.com.local.app2hpam12021;
 //Bibliotecas
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login_Activity extends AppCompatActivity {
+    EditText edtUsuario, edtSenha;
+    TextView txtDesejaCadastrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //vai executar o layout - login_layout
         setContentView(R.layout.login_layout);
+
+        //Apresentar os componentes xml aos objetos no java
+        edtUsuario = findViewById(R.id.edtUsuario);
+        edtSenha = findViewById(R.id.edtSenha);
+        txtDesejaCadastrar = findViewById(R.id.txtDesejaCadastrar);
+
+        //Criando a ação de clique na TextView Deseja cadastrar
+        txtDesejaCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),
+                        CadastrarUsuario_Activity.class));
+                finish();
+            }
+        });
     }
 
     public void entrarJanelaPrincipal(View view) {
         //Mostra uma mensagem para o usuário
-       // Toast.makeText(getApplicationContext(),
-         //       "Cliquei em entrar!!!",Toast.LENGTH_LONG).show();
+        // Toast.makeText(getApplicationContext(),
+        //       "Cliquei em entrar!!!",Toast.LENGTH_LONG).show();
         //Abrir a janela de menu principal
-        Intent abrirJanela = new Intent(getApplicationContext(),Principal_Activity.class);
-        startActivity(abrirJanela);
-        //Fechando a janela de Login
-        finish();
+
+        String usuario, senha;
+
+        usuario = edtUsuario.getText().toString();
+        senha = edtSenha.getText().toString();
+
+        if (usuario.equals("etecia") && senha.equals("etecia")) {
+
+            Intent abrirJanela = new Intent(getApplicationContext(), Principal_Activity.class);
+            startActivity(abrirJanela);
+            //Fechando a janela de Login
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    "Usuário ou senha inválidos!!!",
+                    Toast.LENGTH_SHORT).show();
+        }
 
     }
 
-
+    public void sairJanelaLogin(View view) {
+        finish();
+    }
 }
